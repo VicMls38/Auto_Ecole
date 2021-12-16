89be17f36f98a315cedfa501f749082d9a87b614
VicMls38 <victor.morales@outlook.fr>
2021-12-16 10:46:17 +0100

    cr moniteur

--- controllers/controllerMoniteurs.js
+++ controllers/controllerMoniteurs.js
@@ -7,14 +7,29 @@ var Model = require('../models/modelMoniteurs');
 
 module.exports = {
 
-    Affichage_Moniteurs : (req, res) => {     
+    Page_Moniteurs : (req, res) => {     
         
         Model.Affichage_Moniteurs(function(lignes){
              console.log(lignes);
-             res.render("./testAffichage", {index : lignes});
+             res.render("./moniteur", {index : lignes});
          });
          
      },
+
+     Ajout_Moniteurs : (req, res) => {     
+          
+        res.render("./ajout_moniteur");    
+     },
+
+     Register : (req, res) => {
+        let nom = req.body.nom;
+        let prenom = req.body.prenom;
+        let date_naissance = req.body.date_naissance
+  
+        Model.Inscription_Moniteurs(nom, prenom, date_naissance)
+        res.render("./moniteur")
+    },
+   
    
 
 }
--- routes/routeMoniteurs.js
+++ routes/routeMoniteurs.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const controllerMoniteurs = require('../controllers/controllerMoniteurs');
 
 const Controller = require('../controllers/controllerMoniteurs');
 
 const routeur = express.Router();
 
+
+//routeur.get("/accueil", Controller.Affichage_Moniteurs)
 routeur.get("/testAffichage", Controller.Affichage_Moniteurs)
-routeur.get("/accueil", Controller.Affichage_Moniteurs)
+routeur.get("/moniteur", controllerMoniteurs.Page_Moniteurs)
+
 
 module.exports = routeur
